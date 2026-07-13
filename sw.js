8acb4f7f373abf450587d8a61da46b1740f0cf4d
jonm300 <jonmaso@ymail.com>
2026-07-13 18:30:40 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const C="nanas-bible-complete-v2";
-const A=["./","index.html","manifest.webmanifest"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
